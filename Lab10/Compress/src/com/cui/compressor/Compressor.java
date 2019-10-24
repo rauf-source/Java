e5a6d2976b6f860b3f1582b5069d240850bf3921
Abdul Rauf <raufyy4@gmail.com>
2019-10-24 13:10:03 +0500

    everything is fine till lab 9. Adding undone tasks to lab 10 soon.

--- Lab10/Compress/src/com/cui/compressor/Compressor.java
+++ Lab10/Compress/src/com/cui/compressor/Compressor.java
@@ -1,9 +1,14 @@
+package com.cui.compressor;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.cui.compressor;
+import org.apache.commons.compress.*;
+
+
+
 
 /**
  *
