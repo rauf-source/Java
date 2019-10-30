73ca35ab8b12596b4aa97724748c5573aae7f20f
Abdul Rauf <raufyy4@gmail.com>
2019-10-30 19:45:47 +0500

    lab 14 part 3 started

---
+++ Lab14/Task3/src/com/oop/inheritence/Employee.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.oop.inheritence;
+
+/**
+ *
+ * @author Admin
+ */
+public class Employee {
+    
+}
