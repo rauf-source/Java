78aff16257658fce3e8dfac03cafc3a44a423b3b
ABDUL RAUF <SP19-BSE-043@ciitlahore.edu.pk>
2019-12-02 15:43:01 +0500

    lab19 task 4 done

--- Lab19/Task4/src/com/technocrats/Main.java
+++ Lab19/Task4/src/com/technocrats/Main.java
@@ -9,16 +9,22 @@ package com.technocrats;
  *
  * @author SP19-BSE-043
  */
+import java.util.*;
 public class Main {
 
     /**
      * @param args the command line arguments
      */
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         // TODO code application logic here
         //SUM OF ANY TWO SIDES IS GREATER THAN THE TWO SIDE 
-        Triangle triangle = new Triangle(4,10,4);
-        
+       try{
+        Triangle triangle = new Triangle(input.nextInt(),input.nextInt(),input.nextInt());
+           System.out.println("walalalalala");
+       }catch(IllegalTriangleException exp){
+           System.out.println(exp.getMessage());
+       }
     }
     
 }
